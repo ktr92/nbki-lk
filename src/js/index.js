ed56d61c5326ef3b0cf282327a5f5325d1fe96f8
ktr92 <kudashevtim@gmail.com>
2021-11-17 18:00:28 +0300

    master

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import './maskedinput.min.js'
 
+
+
 $(document).ready(function () {
   $("#checkbox_confirm").click(function () {
     $(this).closest('form').find('.js-next').attr("disabled", !this.checked);
@@ -19,7 +21,27 @@ $(document).ready(function () {
   });
 
 
+  $('.js-toggle').on('click', function (event) {
+    event.preventDefault()
+    $(this).closest('.serv').find('.serv__hidden').slideToggle()
+    $(this).toggleClass('active')
+  });
+
+
   /* $("input[name=date]").mask("99.99.9999");
    */
 
 });
+
+
+(function ($) {
+  $(function () {
+
+    $('.tabs ul').on('click', 'li:not(.active)', function () {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+  });
+})(jQuery);
